d5de4f1a52dfc0b1bfaad34ea09de862357ac645
Usuario <Usuario@DESKTOP-4G5CQFJ>
2022-03-16 18:10:43 -0300

    No anemico

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		
+		Tiempo t= new Tiempo(); 
+		 t.formatoLargo();
+		 t.formatoCorto();
+		 
+		
 	}
 
 }
--- src/Tiempo.java
+++ src/Tiempo.java
@@ -1,7 +1,33 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 
 public class Tiempo {
-
 	
+	private LocalDateTime fechaConHora;
 	
-} 
- 
+	// constructor
+	
+	public Tiempo(){
+		fechaConHora = LocalDateTime.now();
+		}
+	
+	//-------------------------------------------
+	public void formatoLargo() {
+	DateTimeFormatter dtf =
+		    DateTimeFormatter
+		      .ofPattern("EEEE, dd 'de' MMMM 'de' yyyy 'a las' hh:mm:ss");
+	
+		System.out.println("Formato largo: " + fechaConHora.format(dtf));
+	}
+	
+	//-------------------------------------------------------
+	public void formatoCorto() {
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd'/'MM'/'yyyy' a las' hh:mm:ss");
+		
+		System.out.println("Formato corto: " + fechaConHora.format(dtf));
+		
+	}
+}
